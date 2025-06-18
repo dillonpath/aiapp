afa97f6790d9d083023420a001dffb8c3ab71fc2
Dillon Pathammavong <dillonpatha@gmail.com>
2025-06-17 19:43:46 -0500

    19 - added agents filter and synced to react server components

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -6,6 +6,7 @@ import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schema";
+import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
 
@@ -75,11 +76,19 @@ export const meetingsRouter = createTRPCRouter({
             .min(MIN_PAGE_SIZE)
             .max(MAX_PAGE_SIZE)
             .default(DEFAULT_PAGE_SIZE),
-            search: z.string().nullish()
+            search: z.string().nullish(),
+            agentId: z.string().nullish(),
+            status: z.enum([
+                MeetingStatus.UPCOMING,
+                MeetingStatus.ACTIVE,
+                MeetingStatus.PROCESSING,
+                MeetingStatus.COMPLETED,
+                MeetingStatus.CANCELLED,
+            ]).nullish(),
         })
         )
         .query(async ({ ctx,input }) => {
-            const { page, pageSize, search } = input;
+            const { page, pageSize, search, status, agentId } = input;
         const data = await db
             .select({
                 ...getTableColumns(meetings),
@@ -92,6 +101,8 @@ export const meetingsRouter = createTRPCRouter({
                 and(
                     eq(meetings.userId, ctx.auth.user.id),
                     input?.search ? ilike(meetings.name, `%${search}%`) : undefined,
+                    status ? eq(meetings.status, status) : undefined,
+                    agentId ? eq(meetings.agentId, agentId) : undefined,
                 )
             )
             .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -106,6 +117,8 @@ export const meetingsRouter = createTRPCRouter({
                 and(
                     eq(meetings.userId, ctx.auth.user.id),
                     input?.search ? ilike(meetings.name, `%{search}%`) : undefined,
+                    status ? eq(meetings.status, status) : undefined,
+                    agentId ? eq(meetings.agentId, agentId) : undefined,
                 )
             )
         
--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -4,4 +4,10 @@ import type { AppRouter } from "@/trpc/routers/_app";
 
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
 export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
-
+export enum MeetingStatus {
+    UPCOMING = "upcoming",
+    ACTIVE = "active",
+    PROCESSING = "processing",
+    COMPLETED = "completed",
+    CANCELLED = "cancelled",
+}
